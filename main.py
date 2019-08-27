1248b6c4aff30742cced86d9efb7c92ee366d113
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2019-08-27 07:13:39 +0000

    Testing staging pass.

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ service_config = ServiceConfig()
 # @TODO Test
 service_config.LAUNCH_FROM_CONFIG()
 """
+
+"""
 service_config.DETENCT_NOVEL_RELEASE_TAG(
     "github.com:rowsheet/fake_analytics.git",
     "v0.0.5",
 )
-
 """
-# @TODO Test
+
 service_config.PASS_STAGING(
     "github.com:rowsheet/fake_analytics.git",
-    "v0.0.4",
+    "v0.0.5",
 )
-"""
